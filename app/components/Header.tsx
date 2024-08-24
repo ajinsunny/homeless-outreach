0bbcc41d90cd02f145811f0cf2d6451a1187b117
Ajin Sunny <ajin.sunny@gmail.com>
2024-08-23 21:15:30 -0400

    (add) logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <header className="absolute top-0 left-0 right-0 z-50 bg-transparent">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="logo text-white font-bold text-xl md:text-2xl">
-          3rd Saturday Serve in McPherson Square Park
-        </div>
+        <Link href="/" className="logo">
+          <Image
+            src="/logo.webp"
+            alt="3rd Saturday Serve Logo"
+            width={100}
+            height={100}
+            className="w-auto h-12 md:h-16"
+          />
+        </Link>
         <nav className="space-x-4">
           <Link
             href="/"
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Footer from "../app/components/Footer";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "3rd Saturday Serve - McPherson Square Park, DC",
+  title: "3rd Saturday Serve",
   description: "Homeless outreach in McPherson Square Park, DC",
 };
 
